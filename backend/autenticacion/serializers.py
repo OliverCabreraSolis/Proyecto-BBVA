b1ee36d388a19f67eed057fd01504c66c255a2c4
Oliver Cabrera <60933023@continental.edu.pe>
2026-06-12 03:42:29 -0500

    Implementa cambios en autenticación

--- backend/autenticacion/serializers.py
+++ backend/autenticacion/serializers.py
@@ -2,27 +2,43 @@ from rest_framework import serializers
 from .models import Usuarios
 import re
 
+VALIDACIONES_DOCUMENTO = {
+    'DNI': {'min': 8, 'max': 8, 'solo_numeros': True},
+    'RUC': {'min': 11, 'max': 11, 'solo_numeros': True},
+    'Pasaporte': {'min': 6, 'max': 12, 'solo_numeros': False},
+    'Carné de Extranjería': {'min': 9, 'max': 12, 'solo_numeros': False},
+    'Carné Identidad Militar': {'min': 6, 'max': 12, 'solo_numeros': False},
+    'Carné Diplomático': {'min': 6, 'max': 12, 'solo_numeros': False},
+    'Partida de Nacimiento': {'min': 6, 'max': 15, 'solo_numeros': False},
+    'Carné PTP': {'min': 6, 'max': 12, 'solo_numeros': False},
+    'Doc. Identidad País Origen': {'min': 6, 'max': 20, 'solo_numeros': False},
+}
+
 class RegistroSerializer(serializers.Serializer):
     nombre = serializers.CharField(max_length=100)
     apellido = serializers.CharField(max_length=100)
-    tipo_documento = serializers.ChoiceField(choices=['DNI', 'CE', 'Pasaporte'])
+    tipo_documento = serializers.ChoiceField(choices=list(VALIDACIONES_DOCUMENTO.keys()))
     dni = serializers.CharField()
     email = serializers.EmailField()
     password = serializers.CharField()
 
     def validate_dni(self, value):
         tipo = self.initial_data.get('tipo_documento', 'DNI')
-        if tipo == 'DNI':
-            if not value.isdigit():
-                raise serializers.ValidationError('El DNI debe contener solo números.')
-            if len(value) != 8:
-                raise serializers.ValidationError('El DNI debe tener exactamente 8 dígitos.')
-        elif tipo == 'CE':
-            if len(value) < 9 or len(value) > 12:
-                raise serializers.ValidationError('El Carné de Extranjería debe tener entre 9 y 12 caracteres.')
-        elif tipo == 'Pasaporte':
-            if len(value) < 6 or len(value) > 12:
-                raise serializers.ValidationError('El Pasaporte debe tener entre 6 y 12 caracteres.')
+        reglas = VALIDACIONES_DOCUMENTO.get(tipo)
+
+        if not reglas:
+            return value
+
+        if len(value) < reglas['min'] or len(value) > reglas['max']:
+            raise serializers.ValidationError(
+                f'El {tipo} debe tener entre {reglas["min"]} y {reglas["max"]} caracteres.'
+            )
+
+        if reglas['solo_numeros'] and not value.isdigit():
+            raise serializers.ValidationError(
+                f'El {tipo} debe contener solo números.'
+            )
+
         return value
 
     def validate_password(self, value):
